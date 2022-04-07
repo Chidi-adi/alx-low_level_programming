28a2ff0881f3347fe984e0dfc6e440cd5cf906fc
Chidi-adi <101395900+Chidi-adi@users.noreply.github.com>
2022-04-07 17:56:51 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 #include <string.h>
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);	    
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }

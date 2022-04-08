b7f04d87cf7e4ea50db1751f07a2a844a736b0d7
Chidi-adi <nwosuchidiadi@gmail.com>
2022-04-08 06:52:47 -0700

    if_else

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x, y;
+	x = 97;
+	y= 65;
+
+	while(x<=122)
+	{
+	putchar(x);
+	x++;
+	}
+
+	while(y<=90)
+	{
+	putchar(y);
+	y++;
+	}
+
+	putchar('\n');
+	return (0);
+}

f00460cedf3a4a9cd74247e216cf9aa0969f894a
Chidi-adi <101395900+Chidi-adi@users.noreply.github.com>
2022-04-08 15:04:22 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int x;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-x = n%10;
+x = n % 10;
 
 if (x > 0)
 	{
@@ -26,7 +26,7 @@ else if (x == 0)
 	}
 else
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);	
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 	}
 return (0);
 }

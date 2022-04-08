c0c499e859962cc2ba75dd290069ae4036517acd
Chidi-adi <101395900+Chidi-adi@users.noreply.github.com>
2022-04-08 21:58:25 +0100

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x = 48;
+
+while (x <= 57)
+{
+int y = 48;
+
+while(y <= 57)
+{
+if (x != y && x < y)
+{
+putchar(x);
+putchar(y);
+if (x == 56 && y == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+y++;
+}
+x++;  
+}
+putchar('\n');
+return (0);
+}

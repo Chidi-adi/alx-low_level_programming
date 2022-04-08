87ff7b1b964442a84d1f4d4e4265ec20b005d24e
Chidi-adi <101395900+Chidi-adi@users.noreply.github.com>
2022-04-08 16:05:11 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,13 @@
  */
 int main(void)
 {
-	int i=0;
+int i = 0;
 
-	while(i<10)
-	{
-		putchar(i%10 + '0');
-		i++;
-	}
-	return (0);
+while (i < 10)
+{
+putchar(i%10 + '0');
+i++;
+}
+putchar('\n');
+return (0);
 }

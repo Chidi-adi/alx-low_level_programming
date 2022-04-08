e6ccbe121eeecb7dd149579cdc6ccee021e88157
Chidi-adi <101395900+Chidi-adi@users.noreply.github.com>
2022-04-08 21:40:47 +0100

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x = 48;
+int y = 97;
+
+while (x <= 57)
+{
+putchar(x);
+x++;
+}
+
+while (y <= 102)
+{
+putchar(y);
+y++;
+}
+
+putchar('\n');
+return (0);
+}

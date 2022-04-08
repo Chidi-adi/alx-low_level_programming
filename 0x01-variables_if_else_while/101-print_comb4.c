ceea820c3fd6ae904c440215918adecdc7a109df
Chidi-adi <101395900+Chidi-adi@users.noreply.github.com>
2022-04-08 22:07:31 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,18 +13,18 @@ while (x <= 57)
 int y = 48;
 
 while (y <= 57)
-{ 
+{
 int z = 48;
 
 while (z <= 57)
-{ 
+{
 if (x != y && y != z && x != z && x < y && y < z)
 {
 putchar(x);
 putchar(y);
 putchar(z);
 
-if(x == 55 && y == 56 && z == 57)
+if (x == 55 && y == 56 && z == 57)
 {
 break;
 }

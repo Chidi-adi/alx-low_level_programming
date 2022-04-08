092a4f2f31d9a04eceb35b427c7ef819c86e1411
Chidi-adi <101395900+Chidi-adi@users.noreply.github.com>
2022-04-08 23:31:35 +0100

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a, b, c, d, op1, op2;
+a = 48;
+
+for (a; a <= 57; a++)
+{
+b = 48;
+
+for (b; b <= 57; b++)
+{
+c = 48;
+
+for (c; c <= 57; c++)
+{
+d = 48;
+
+for (d; d <= 57; d++)
+{
+op1 = a * 10 + b;
+op2 = c * 10 + d;
+
+if (op1 < op2)
+{
+putchar(a);
+putchar(b);
+  
+ 
+  
+                       putchar(' ');
+                                 putchar(c);
+                                 putchar(d);
+                                     if (a == 57 && b ==56 && c == 57 && d == 57)
+
+                                     {
+
+                                       
+                                         break;
+
+                                     }
+
+                                 putchar(',');
+
+                                 putchar(' ');
+
+                             }
+
+                         }
+
+                 }
+
+             }
+
+        }
+
+    putchar('\n');
+
+return (0);
+
+}

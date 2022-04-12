7255e471b400de3fd8c422f476f73e65dc92f3fa
Chidi-adi <101395900+Chidi-adi@users.noreply.github.com>
2022-04-12 23:42:48 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,7 @@
  */
 int main(void)
 {
-    char ch[] = {95, 112, 117, 116, 99, 104, 97, 114};
-    int i;
-    i = 0;
-    
-    for (i; i <= 7; i++)
-    {
-    _putchar(ch[i]);
-    }
-    
-    _putchar('\n');
-    return (0);
+    printf("_putchar");
+    Return (0);
+   
 }

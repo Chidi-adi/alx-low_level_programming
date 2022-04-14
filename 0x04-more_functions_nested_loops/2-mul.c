e4cd3e793e9e00c9ba137ad3e091f8bc7c8ef98e
Chidi-adi <101395900+Chidi-adi@users.noreply.github.com>
2022-04-14 16:33:08 +0100

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+ /*
+ * File: 0-isupper.c
+ * Auth: Chidiadi Nwosu
+ */
+
+#include "main.h"
+
+/**
+ * mul - multiplies two integers..
+ * @a, @b: The characters to be multiplied.
+ *
+ * Return: a * b.
+ */
+int mul(int a, int b)
+{
+		return (a * b);
+}

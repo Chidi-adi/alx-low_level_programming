30ca8657b30e598d147f0dcd859c124993badbd3
Chidi-adi <nwosuchidiadi@gmail.com>
2022-04-14 09:02:11 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+ /*
+ * File: 3-print_numbers.c 
+ * Auth: Chidiadi Nwosu
+ */
+
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9.
+ *
+ * Return: 0 Always.
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+		_putchar(a);
+
+	_putchar('\n');
+	return (0);
+}
+

2dd4588ca495e034af778be2b71f7157bcc00b75
Chidi-adi <101395900+Chidi-adi@users.noreply.github.com>
2022-04-14 18:39:56 +0100

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+/*
+ * File: 6-print_line.c
+ * Auth: Chidiadi Nwosu
+ */
+
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
+ */
+void print_line(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
